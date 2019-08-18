28c7cb2c285014c4f94c2ee29036384b5f7c91f0
Anthony <anthonymiller313@hotmail.com>
2019-08-18 14:35:45 -0400

    fixed link error in login.php

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
     </head>
     <body>
         <h2>Login Page</h2>
-        <a href="index.php">Click here to go back<br/><br/>
+        <a href="index.php">Click here to go back</a><br/><br/>
         <form action="checklogin.php" method="POST">
            Enter Username: <input type="text" name="username" required="required" /> <br/>
            Enter password: <input type="password" name="password" required="required" /> <br/>
